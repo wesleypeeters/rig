ddc43be0f3190abe5f68a79455ccecaa52a67771
Wesley Peeters <wesley-peeters@live.nl>
2026-03-27 18:09:56 +0000

    lowercase registry path from GITHUB_REPOSITORY

--- src/stack/getServiceTag.ts
+++ src/stack/getServiceTag.ts
@@ -2,7 +2,7 @@ import name from "./name.ts";
 import { optional } from "../util/env.ts";
 import { currentBranchSlug } from "../github/pr.ts";
 
-const registry = optional.GITHUB_REGISTRY || (optional.GITHUB_REPOSITORY ? `ghcr.io/${optional.GITHUB_REPOSITORY}` : `${name}.local`);
+const registry = optional.GITHUB_REGISTRY || (optional.GITHUB_REPOSITORY ? `ghcr.io/${optional.GITHUB_REPOSITORY.toLowerCase()}` : `${name}.local`);
 
 export default function (service: string): string {
 	return `${registry}/${service}:${currentBranchSlug}`;
